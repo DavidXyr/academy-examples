e76d9de04a38f6c416298d6e18f4441550c2b087
Harri Sarsa <harri.sarsa@appgyver.com>
2013-06-25 16:00:21 +0300

    updated animations without navigation tutorial

--- native_ui/animations_without_navigation/dist/javascripts/application.js
+++ native_ui/animations_without_navigation/dist/javascripts/application.js
@@ -1,3 +1,21 @@
+var animation = new steroids.Animation( {
+ transition: "curlUp",
+ duration: 0.8,
+ curve: "easeIn"
+} );
 
-// steroids.view.navigationBar.show("Hello World");
+var times = 0;
 
+function animationSuccess() {
+ times++;
+ document.getElementById("status").textContent = "Animation performed " + times + " times!";
+}
+
+function performAnimation() {
+
+ animation.perform(
+   {}, 
+   { onSuccess: animationSuccess }
+ );
+
+}
