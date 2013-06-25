2347fdae277ad6edc431d56d2220f7a2febd16bf
Harri Sarsa <harri.sarsa@appgyver.com>
2013-06-25 13:02:52 +0300

    navigation bar example topcoated

--- native_ui/navigation_bar/dist/javascripts/application.js
+++ native_ui/navigation_bar/dist/javascripts/application.js
@@ -1,3 +1,36 @@
+steroids.view.navigationBar.show("Phoenix Foundation");
 
-// steroids.view.navigationBar.show("Hello World");
+// Set up our login text element
+var loginText = null;
 
+document.addEventListener("DOMContentLoaded", function() {
+  loginText = document.querySelector("#login-text");
+});
+
+// Login and logout buttons
+
+var loginButton = new steroids.buttons.NavigationBarButton();
+loginButton.title = "Log in";
+
+var logoutButton = new steroids.buttons.NavigationBarButton();
+logoutButton.title = "Log out";
+
+loginButton.onTap = function() {
+  loginText.textContent = "Welcome, Agnus M.!";
+  steroids.view.navigationBar.setButtons({
+    right: [logoutButton]
+  });
+};
+
+logoutButton.onTap = function() {
+  loginText.textContent = "Please log in.";
+  steroids.view.navigationBar.setButtons({
+    right: [loginButton]
+  });
+}
+
+// Initially display the login button
+
+steroids.view.navigationBar.setButtons({
+  right: [loginButton]
+});
