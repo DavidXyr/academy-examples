cee31e670f816ba8d2148cb76e667cbf288cb5da
Harri Sarsa <harri.sarsa@appgyver.com>
2013-08-22 12:50:34 +0300

    name change to modal_screen, updated to steroids.js 2.7.7 and npm 2.7.17 project structure, removed dist/

--- native_ui/modal_window/www/javascripts/application.js
+++
@@ -1,9 +0,0 @@
-function showModal() {
-  var webView = new steroids.views.WebView("modal.html");
-  steroids.modal.show(webView);
-}
-
-function previewDolanImage() {
-  var fileView = new steroids.views.PreviewFileView("images/dolan.png");
-  steroids.modal.show(fileView);
-}
